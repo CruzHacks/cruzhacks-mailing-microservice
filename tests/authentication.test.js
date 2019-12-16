ff132a7f57735c53e12fbefa224d21bc47e9e98a
kyle1668 <kyledevinobrien1@gmail.com>
2019-12-15 17:56:08 -0800

    copied over tests for auth middleware

---
+++ tests/authentication.test.js
@@ -0,0 +1,33 @@
+const { authenticateApiKey } = require("../mailing/authentication");
+
+describe("api key authentication middleware uit tests", () => {
+  const contextMock = {};
+
+  test("should pass with correct request header", () => {
+    const requestHeadersMock = {
+      headers: {
+        accept: "*/*",
+        host: "localhost:7071",
+        "user-agent": "insomnia/7.0.3",
+        authentication: "testKEY",
+      },
+      query: {},
+    };
+    process.env.API_KEY = "testKEY";
+    expect(authenticateApiKey(contextMock, requestHeadersMock)).toBe(true);
+  });
+
+  test("should not pass with incorrect request header", () => {
+    const requestHeadersMock = {
+      headers: {
+        accept: "*/*",
+        host: "localhost:7071",
+        "user-agent": "insomnia/7.0.3",
+        authentication: "WRONG_KEY",
+      },
+      query: {},
+    };
+    process.env.API_KEY = "testKEY";
+    expect(authenticateApiKey(contextMock, requestHeadersMock)).toBe(false);
+  });
+});
