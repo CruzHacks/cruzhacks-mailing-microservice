59af04b2beb7043384971267ad65be503a097143
Isai Lopez Rodas <40477553+ilopezro@users.noreply.github.com>
2020-09-23 11:03:24 -0700

    Transition from Azure to Firebase (#9)
    * used mailchimp api to get mailing service running

--- functions/index.js
+++ functions/index.js
@@ -4,36 +4,49 @@ const { authenticateApiKey, parseEmailFromRequest } = require("./mailing/middlew
 const { addToMailingList } = require("./mailing/mailchimp");
 
 exports.subscribe = functions.https.onRequest(async (request, response) => {
-  // TODO: refactor tests with firebase changes
-
-  const isAuthenticated = authenticateApiKey(functions, request);
-  const requestEmail = parseEmailFromRequest(request);
-
-  if (isAuthenticated === false) {
-    response.status(401).send({
-      error: true,
-      status: 401,
-      message: "Unable to authenticate request",
-    });
-  } else if (requestEmail === null) {
-    response.status(400).send({
-      error: true,
-      status: 400,
-      message: "Invalid or missing email in request body",
-    });
+  response.set("Access-Control-Allow-Origin", "https://www.cruzhacks.com");
+  if (request.method === "OPTIONS") {
+    response.set("Access-Control-Allow-Methods", "POST");
+    response.set("Access-Control-Allow-Headers", "authentication, Content-Type");
+    response.status(200).send('');
   } else {
-    await addToMailingList(functions, requestEmail).catch(error => {
-      response.status(500).send({
-        error: true,
-        status: 500,
-        message: error.message,
-      });
+    const isAuthenticated = authenticateApiKey(functions, request);
+    const requestEmail = parseEmailFromRequest(request);
 
-      response.status(200).send({
-        error: false,
-        status: 200,
-        message: `${requestEmail} added to the mailing list`,
+    if (isAuthenticated === false) {
+      response.status(401).send({
+        error: true,
+        status: 401,
+        message: "Unable to authenticate request",
       });
-    });
+    } else if (requestEmail === null) {
+      response.status(400).send({
+        error: true,
+        status: 400,
+        message: "Invalid or missing email in request body",
+      });
+    } else {
+      await addToMailingList(functions, requestEmail).then((res) => {
+        if (res.status === 200) {
+          return response.status(200).send({
+            error: false,
+            status: 200,
+            message: `${requestEmail} is already subscribed`,
+          });
+        } else {
+          return response.status(201).send({
+            error: false,
+            status: 201,
+            message: `${requestEmail} added to the mailing list`,
+          });
+        }
+      }).catch(error => {
+        response.status(500).send({
+          error: true,
+          status: 500,
+          message: error.message,
+        });
+      });
+    }
   }
 });
--- functions/mailing/mailchimp.js
+++ functions/mailing/mailchimp.js
@@ -1,38 +1,53 @@
-const axios = require("axios");
 const functions = require('firebase-functions');
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 const mailchimpConfiguration = {
   authKey: functions.config().subscribe.mailchimp_api_key,
-  endpoint: functions.config().subscribe.mailchimp_subscription_endpoint,
+  server: functions.config().subscribe.mailchimp_server,
+  user: functions.config().subscribe.mailchimp_user_name,
 };
+
+mailchimp.setConfig({
+  apiKey: mailchimpConfiguration.authKey,
+  server: mailchimpConfiguration.server
+});
 
 const requestConfiguration = {
-  user: {
-    key: mailchimpConfiguration.authKey,
+  auth: {
+    user: mailchimpConfiguration.user,
+    password: mailchimpConfiguration.authKey,
   },
-  headers: {
+  header: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin": "*"
   },
 };
 
-const addToMailingList = (email) => {
-  const requestBody = {
-    email_address: email,
-    status: "subscribed",
-  };
-
-  return axios
-    .post(mailchimpConfiguration.endpoint, requestBody, requestConfiguration)
-    .then((response) => {
-      functions.logger.info(JSON.stringify(response.data, null, 2));
-      return Promise.resolve(true);
-    })
-    .catch((error) => {
-      functions.logger.error(error.response.data.detail);
-      return Promise.reject(new Error(`Mailchimp Error: ${error.response.data.detail}`));
+async function addToMailingList(functions, email) {
+  const response = await mailchimp.lists.addListMember("776764ba24", {
+    email_address: email, 
+    status: "subscribed"
+  }).then((response) => {
+    functions.logger.info(response.id);
+    return Promise.resolve({
+      message: `Successfully added ${email} to the emailing list`,
+      status: 201,
     });
-};
+  }).catch((error) => {
+    const res = error.response.body.title;
+    if(res.includes("Member Exists")) {
+      functions.logger.info(JSON.stringify(res, null, 2));
+      return Promise.resolve({
+        message: `${email} already exists`,
+        status: 200,
+      });
+    } else {
+      functions.logger.error(JSON.stringify(res, null, 2));
+      return Promise.reject(new Error(`Mailchimp Error: ${res}`));
+    }
+  });
+  return response;
+}
 
 module.exports = {
   addToMailingList,
