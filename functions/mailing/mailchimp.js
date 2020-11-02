76a57cd5337e9c7ae28e9dd76b81e55ecab9f919
ilopezro <ilopezro@ucsc.edu>
2020-11-01 21:00:48 -0800

    added linter and removed unnecessary dependencies

--- functions/mailing/mailchimp.js
+++ functions/mailing/mailchimp.js
@@ -1,4 +1,4 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 const mailchimpConfiguration = {
@@ -9,7 +9,7 @@ const mailchimpConfiguration = {
 
 mailchimp.setConfig({
   apiKey: mailchimpConfiguration.authKey,
-  server: mailchimpConfiguration.server
+  server: mailchimpConfiguration.server,
 });
 
 const requestConfiguration = {
@@ -19,33 +19,36 @@ const requestConfiguration = {
   },
   header: {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*"
+    "Access-Control-Allow-Origin": "*",
   },
 };
 
 async function addToMailingList(functions, email) {
-  const response = await mailchimp.lists.addListMember("776764ba24", {
-    email_address: email, 
-    status: "subscribed"
-  }).then((response) => {
-    functions.logger.info(response.id);
-    return Promise.resolve({
-      message: `Successfully added ${email} to the emailing list`,
-      status: 201,
-    });
-  }).catch((error) => {
-    const res = error.response.body.title;
-    if(res.includes("Member Exists")) {
-      functions.logger.info(JSON.stringify(res, null, 2));
+  const response = await mailchimp.lists
+    .addListMember("776764ba24", {
+      email_address: email,
+      status: "subscribed",
+    })
+    .then(response => {
+      functions.logger.info(response.id);
       return Promise.resolve({
-        message: `${email} already exists`,
-        status: 200,
+        message: `Successfully added ${email} to the emailing list`,
+        status: 201,
       });
-    } else {
-      functions.logger.error(JSON.stringify(res, null, 2));
-      return Promise.reject(new Error(`Mailchimp Error: ${res}`));
-    }
-  });
+    })
+    .catch(error => {
+      const res = error.response.body.title;
+      if (res.includes("Member Exists")) {
+        functions.logger.info(JSON.stringify(res, null, 2));
+        return Promise.resolve({
+          message: `${email} already exists`,
+          status: 200,
+        });
+      } else {
+        functions.logger.error(JSON.stringify(res, null, 2));
+        return Promise.reject(new Error(`Mailchimp Error: ${res}`));
+      }
+    });
   return response;
 }
 
